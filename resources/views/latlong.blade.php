4ae35c71034e96db5b2f2024c039c42cb414443d
song <zosong@126.com>
2020-09-28 15:19:49 +0800

    laravel-admin 2.0

--- resources/views/latlong.blade.php
+++ resources/views/latlong.blade.php
@@ -5,10 +5,10 @@
     <div class="{{$viewClass['field']}}">
         <div class="row">
             <div class="col-3">
-                <input id="{{$id['lng']}}" name="{{$name['lng']}}" class="form-control" value="{{ old($column['lng'], $value['lng']) }}" {!! $attributes !!} />
+                <input id="{{$id['lat']}}" name="{{$name['lat']}}" class="form-control" value="{{ old($column['lat'], $value['lat']) }}" {!! $attributes !!} />
             </div>
             <div class="col-3">
-                <input id="{{$id['lat']}}" name="{{$name['lat']}}" class="form-control" value="{{ old($column['lat'], $value['lat']) }}" {!! $attributes !!} />
+                <input id="{{$id['lng']}}" name="{{$name['lng']}}" class="form-control" value="{{ old($column['lng'], $value['lng']) }}" {!! $attributes !!} />
             </div>
 
             @if($provider != 'yandex')
